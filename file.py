9a4a52b0bac1f07ca133e4921b94af76f1e0873b
Chlpn <prem@isi.ae>
2019-09-21 23:04:41 +0400

    prem
    prem for invoice to cancel only by adviser

---
+++ file.py
@@ -0,0 +1,17 @@
+
+
+   class AccountInvoice(models.Model):
+   _inherit = "account.invoice"
+   
+   @api.model
+   def action_cancel(self):
+    if self.env['res.users'].has_group('account.group_account_manager'):
+      rec= return super(TransMaster, self).unlink()
+    else:
+      raise UserError(_('You are not privilaged to cancel this invoice'))
+      rec=0   
+       
+    return rec
+        
+        
+       
