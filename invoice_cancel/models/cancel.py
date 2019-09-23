820bcb9d7b7d38c90fea91d63027c3efd5807152
harilal <harilal@isi.ae>
2019-09-23 14:27:53 +0400

    .

--- invoice_cancel/models/cancel.py
+++ invoice_cancel/models/cancel.py
@@ -6,7 +6,7 @@ class AccountInvoice(models.Model):
    @api.model
    def action_cancel(self):
     if self.env['res.users'].has_group('account.group_account_manager'):
-      rec= return super(TransMaster, self).unlink()
+      rec= super(AccountInvoice, self).unlink()
     else:
       raise UserError(_('You are not privilaged to cancel this invoice'))
       rec=0   
