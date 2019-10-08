3d1828cbada70830beb1b5a9f0f28c77d28ec307
harilal <harilal@isi.ae>
2019-10-08 20:57:39 +0530

    .

---
+++ deliverynote_description/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Delivery note description',
+    'summary': 'Adding Description field for delivery note.',
+    'version': '10.0',
+    'author': "iSquare Informatics",
+    'category': 'sales',
+    'depends': ['sale','stock'],
+    'website': 'https://isi.ae',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'data': ['views\deliverynote_description_view.xml'],
+}
