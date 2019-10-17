be988551c3152713d8d795089389422997777555
harilal <harilal@isi.ae>
2019-10-17 21:43:21 +0530

    .

--- bi_professional_reports_templates/res_company.py
+++ bi_professional_reports_templates/res_company.py
@@ -47,12 +47,12 @@ class account_invoice(models.Model):
         self.sent = True
         return self.env['report'].get_action(self, 'bi_professional_reports_templates.report_invoice')
 
-    @api.multi
-    def amount_to_text(self, amount, currency='AED'):
-        convert_amount_in_words = amount_to_text_en.amount_to_text(amount, lang='en', currency='')
-        convert_amount_in_words = convert_amount_in_words.replace(' and Zero Cent', ' Only ')
-        convert_amount_in_words = convert_amount_in_words.replace('Cents', 'Fils')
-        return convert_amount_in_words
+@api.multi
+def amount_to_text(self, amount, currency='AED'):
+    convert_amount_in_words = amount_to_text_en.amount_to_text(amount, lang='en', currency='')
+    convert_amount_in_words = convert_amount_in_words.replace(' and Zero Cent', ' Only ')
+    convert_amount_in_words = convert_amount_in_words.replace('Cents', 'Fils')
+    return convert_amount_in_words
 
 
 class res_company(models.Model):
